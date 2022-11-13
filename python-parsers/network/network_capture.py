940dbfcae8ee2e80c0841b2c0dd9083c36790791
Adam Siwiec <73085927+adamsiwiec1@users.noreply.github.com>
2022-11-13 17:19:31 -0500

    Delete python-parsers directory

--- python-parsers/network/network_capture.py
+++
@@ -1,8 +0,0 @@
-import pyshark
-
-capture = pyshark.LiveCapture(interface='eth0')
-capture.sniff(timeout=10,packet_count=10)
-
-while True:
-    for packet in capture.sniff_continuously(packet_count=5):
-        print('Just arrived:', packet)
